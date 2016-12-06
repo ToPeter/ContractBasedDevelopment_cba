5b7140b8857985de0b347517edf6aed584708024
topeter <tomascik.peter@gmail.com>
2016-12-06 17:43:22 +0100

    C# class that implements Design by Contract in C#

---
+++ ContractCode/Account.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace ContractCode
+{
+    class Account {
+
+        //public Account(double balance)
+        //{
+        //    Contract.Requires(balance >= 0.0);
+        //    this.balance = balance;
+        //}
+        private double _balance ;
+        public double balance
+        {
+            get { return _balance; }
+
+            set
+            {
+
+                _balance = balance;
+                Contract.Requires(_balance >= 0.0);
+            }
+
+        }
+
+
+        public double Deposit(double amount)
+        {
+            _balance += amount;
+            return _balance;
+        }
+
+
+        public double Withdraw(double amount)
+        {
+            //this.balance -= amount;
+            //return this.balance;
+
+            _balance += amount;
+            return _balance;
+        }
+
+        public void checkBalance(double balance)
+        {
+            Contract.Requires(balance >= 0.0);
+        }
+
+
+    }
+}
---
+++ ContractCode/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace ContractCode
+{
+    class Program
+    {
+        private static Account a1;
+        private static Account a2;
+
+        private static void Main(string[] args)
+        {
+            a1 = new Account();
+            a2 = new Account();
+
+            a1.balance = -50.00;
+            a2.balance = -500.00;
+
+            //Console.WriteLine("This is initial balance: " + a1.balance);
+            //double returnBalanceWithdraw = a1.Withdraw(10.0);
+            //a1.checkBalance(returnBalanceWithdraw);
+            //Console.WriteLine("This is after first withdraw: " + returnBalanceWithdraw);
+
+            a1.checkBalance(a1.Withdraw(10.0));
+            
+            double returnBalanceDeposit1 = a1.Deposit(10.0);
+            a1.checkBalance(returnBalanceDeposit1);
+            Console.WriteLine("This is after deposit: " + returnBalanceDeposit1);
+
+            //while (true)
+            //{
+
+            //}
+
+        }
+    }
+}
+
+
